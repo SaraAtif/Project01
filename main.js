398c3882d8a199b0996f5886e909e23b48afcd7c
Sara <saraatif135@gmail.com>
2024-04-18 00:00:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Ques#1
+//Install Node.js, Typescript and vs code
+//Ques#2
+var name1 = "Aaliyan";
+var message = "Have you ever seen London?";
+console.log("Hello" + ' ' + name1 + '! ' + message);
+//Ques#3
+var userName = "Tauseen";
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,14 @@
+// Ques#1
+
+//Install Node.js, Typescript and vs code
+
+//Ques#2
+let name1 ="Aaliyan";
+let message = "Have you ever seen London?";
+console.log("Hello" +' ' + name1 + '! ' + message);
+
+//Ques#3
+let userName = "Tauseen";
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase())
